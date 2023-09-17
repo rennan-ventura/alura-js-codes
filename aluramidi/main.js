20d4ed54defa9d132f31a882bf8f1adbe1e05c60
Rennan Ventura <rennan.ventura.alves@gmail.com>
2023-09-17 01:06:05 -0300

    Primeiro curso finalizado

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,21 +1,38 @@
-function playSong(idElementAudio) {
+function playSong(AudioSelector) {
 
-    document.querySelector(idElementAudio).play();
+    const elemento = document.querySelector(AudioSelector);
+
+    if(elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 
 }
 
 const keyList = document.querySelectorAll('.tecla');
 
-
 for(let count = 0; count < keyList.length; count++) {
 
     const key = keyList[count];
     const instrument =  key.classList[1];
-
     const idAudio = `#som_${instrument}`;
 
     key.onclick = function ()  {
         playSong(idAudio);
+    }
+
+    key.onkeydown = function (event) {
+
+        console.log(event);
+
+        if(event.code === 'Space' || event.code === 'Enter'){
+            key.classList.add('ativa');
+        }
+    }
+    key.onkeyup = function () {
+        key.classList.remove('ativa');
     }
 
 }
